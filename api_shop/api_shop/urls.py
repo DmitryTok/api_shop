04f9bb17ad66a5e400030a1f9eb579f0ff0ee548
DmitryTok <FoB09@ukr.net>
2026-02-19 18:55:55 +0100

    add api for profile

--- api_shop/api_shop/urls.py
+++ api_shop/api_shop/urls.py
@@ -11,6 +11,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/profiles/', include('profiles.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
