69f5c146ec247aed7367cb34a611e3c9ba413e20
DmitryTok <FoB09@ukr.net>
2026-02-22 17:04:49 +0100

    add auth by phone

--- api_shop/api_shop/urls.py
+++ api_shop/api_shop/urls.py
@@ -7,17 +7,17 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView
 )
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+from users.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/profiles/', include('profiles.urls')),
     path(
-        'api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'
+        'api/login/',
+        CustomTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
     ),
     path(
         'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
