465c0906138099207884478039574e147c2d7511
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-04-29 13:18:21 +0300

    Register Category in admin

--- api_shop/categories/admin.py
+++ api_shop/categories/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "is_active", "is_hidden", "created_at")
+    search_fields = ("name", "slug")
+    list_filter = ("is_active", "is_hidden")
