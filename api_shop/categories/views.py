4c07ba8ede04de4a8af3c79ae4687f8cfafd9f32
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-04-29 12:52:24 +0300

    Clean ViewSet: remove filters and extra logic

--- api_shop/categories/views.py
+++ api_shop/categories/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.serializers import CategorySerializer
-from django_filters.rest_framework import DjangoFilterBackend
-from .filters import CategoryFilter
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = "slug"
-
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = CategoryFilter
-
-def get_queryset(self):
-    qs = super().get_queryset()
-    print("FILTER PARAMS:", self.request.query_params)
-    return qs
+    
