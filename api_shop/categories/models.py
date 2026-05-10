2dea4c7501d1f887332369196a15f16b1630f0d8
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-05-10 18:09:34 +0300

    Remove parent subcategory migration
    :wq:wq#

--- api_shop/categories/models.py
+++ api_shop/categories/models.py
@@ -2,34 +2,28 @@ from django.db import models
 from django.utils.text import slugify
 from addons.mixins import TimeStampMixin
 
+
 class Category(TimeStampMixin):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(unique=True)
+
     is_active = models.BooleanField(default=True)
     is_hidden = models.BooleanField(default=False)
-    
-    parent = models.ForeignKey(
-        "self",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name="children"
-    )
 
     def save(self, *args, **kwargs):
         if self.name:
-           self.name = self.name.strip()
+            self.name = self.name.strip()
 
         if not self.slug:
-           base_slug = slugify(self.name)
-           slug = base_slug
-           counter = 1
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
 
-           while Category.objects.filter(slug=slug).exists():
-               slug = f"{base_slug}-{counter}"
-               counter += 1
+            while Category.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
 
-           self.slug = slug
+            self.slug = slug
 
         super().save(*args, **kwargs)
 
@@ -37,6 +31,45 @@ class Category(TimeStampMixin):
         return self.name
 
     class Meta:
-         verbose_name = "Category"
-         verbose_name_plural = "Categories"
-         ordering = ["-created_at"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ["-created_at"]
+
+
+class Subcategory(TimeStampMixin):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="subcategories"
+    )
+
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_hidden = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.name:
+            self.name = self.name.strip()
+
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            while Subcategory.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Subcategory"
+        verbose_name_plural = "Subcategories"
+        ordering = ["-created_at"]
