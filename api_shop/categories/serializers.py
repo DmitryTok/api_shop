34a9bf7682bff16129136c119c016c043ad4ff77
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-05-11 21:42:13 +0300

    Refactor category slug generation

--- api_shop/categories/serializers.py
+++ api_shop/categories/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Category
-from addons.slugify import generate_slug
+
 
 class CategorySerializer(serializers.ModelSerializer):
     slug = serializers.CharField(required=False)
+    
 
     def validate_name(self, value):
         if not value:
@@ -18,19 +19,7 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = "__all__"
 
-    def create(self, validated_data):
-        name = validated_data.get("name")
-
-        if not validated_data.get("slug"):
-           validated_data["slug"] = generate_slug(name)
-
-        return super().create(validated_data)
     
-    def update(self, instance, validated_data):
-        name = validated_data.get("name")
-
-        if name and name != instance.name:
-           validated_data["slug"] = generate_slug(name)
-
-        return super().update(instance, validated_data)
+    
+  
 
