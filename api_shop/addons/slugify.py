b984d776ad403bcf8008a6563f9885e1bcbafd82
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-05-12 20:45:05 +0300

    Improve slug helper typing

--- api_shop/addons/slugify.py
+++ api_shop/addons/slugify.py
@@ -1,7 +1,8 @@
 from django.utils.text import slugify
+from django.db.models import Model
 
 
-def generate_unique_slug(model, value: str) -> str:
+def generate_unique_slug(model: type[Model], value: str) -> str:
     base_slug = slugify(value)
     slug = base_slug
     counter = 1
