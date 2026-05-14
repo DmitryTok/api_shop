8b47dcdf06474deb5a0fcc03db51f04e8e37a00e
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-05-14 21:42:55 +0300

    Add subcategory endpoints and serializers

--- api_shop/categories/views.py
+++ api_shop/categories/views.py
@@ -1,10 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
-from categories.models import Category
-from categories.serializers import CategorySerializer
+
+from categories.models import Category, Subcategory
+from categories.serializers import (
+    CategorySerializer,
+    SubcategorySerializer,
+)
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = "id"
+
+
+class SubcategoryViewSet(ModelViewSet):
+    queryset = Subcategory.objects.select_related("category")
+    serializer_class = SubcategorySerializer
     lookup_field = "id"
     
