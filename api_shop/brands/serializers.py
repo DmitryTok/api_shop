1bc9626e1fa01bd7c242901ff9acbdb57bd31824
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-05-20 21:00:21 +0300

    Add auto slug generation for brands

--- api_shop/brands/serializers.py
+++ api_shop/brands/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
 from .models import Brand
-
+from addons.slugify import generate_unique_slug
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = "__all__"
+
+        def create(self, validated_data):
+           
+           if not validated_data.get("slug"):
+                 validated_data["slug"] = generate_unique_slug(
+                  Brand,
+                 validated_data["name"]
+              )
+
+                 return super().create(validated_data)
+
