29c35ffe3e721f1baf00223b9132e13fa92cb638
DmitryTok <FoB09@ukr.net>
2026-05-27 14:58:52 +0200

    add cors settings

--- api_shop/api_shop/settings.py
+++ api_shop/api_shop/settings.py
@@ -23,6 +23,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = True
@@ -39,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'corsheaders' 'rest_framework',
     'rest_framework_simplejwt',
     'drf_spectacular',
     'debug_toolbar',
@@ -49,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
