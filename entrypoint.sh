1fc506a61865701ecef693992f8bb5b5efecaf1c
DmitryTok <FoB09@ukr.net>
2026-06-02 09:04:54 +0200

    fixes entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ echo "--> Create DB Tables..."
 python api_shop/manage.py migrate
 
 echo "--> Create ADMIN User"
-python api_shop/manage.py create_default_superuser || echo "Суперпользователь уже существует или ошибка создания"
+python api_shop/manage.py create_default_superuser || echo "ADMIN User already exists"
 
 echo "--> Start Gunicorn..."
-exec /opt/venv/bin/gunicorn --bind 0.0.0.0:$PORT --chdir /app api_shop.wsgi:application
+
+exec /opt/venv/bin/gunicorn --bind 0.0.0.0:$PORT --chdir /app/api_shop api_shop.wsgi:application
