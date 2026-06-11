c7463b9813e5474809172c5be1cdd6ed31f4f489
DmitryTok <FoB09@ukr.net>
2026-06-11 15:08:23 +0200

    fix profile json

--- api_shop/profiles/serializers.py
+++ api_shop/profiles/serializers.py
@@ -1,9 +1,16 @@
-from profiles.models import Profile
+from profiles.models import ClothingSize, Gender, Profile
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from users.serializers import UserSerializer
 
 
 class ProfileSerializer(ModelSerializer):
+    gender = serializers.ChoiceField(
+        choices=Gender.choices(), source='get_gender_display'
+    )
+    clothing_size = serializers.ChoiceField(
+        choices=ClothingSize.choices(), source='get_clothing_size_display'
+    )
     user = UserSerializer(read_only=True)
 
     class Meta:
