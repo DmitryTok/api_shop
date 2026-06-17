b681a617c74aea44a7a466de757283a638c3f87f
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-06-17 22:01:28 +0300

    Remove slug generation from category models

--- api_shop/categories/models.py
+++ api_shop/categories/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.text import slugify
 from addons.mixins import TimeStampMixin
 
 
@@ -10,22 +9,7 @@ class Category(TimeStampMixin):
     is_active = models.BooleanField(default=True)
     is_hidden = models.BooleanField(default=False)
 
-    def save(self, *args, **kwargs):
-        if self.name:
-            self.name = self.name.strip()
-
-        if not self.slug:
-            base_slug = slugify(self.name)
-            slug = base_slug
-            counter = 1
-
-            while Category.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-
-            self.slug = slug
-
-        super().save(*args, **kwargs)
+   
 
     def __str__(self):
         return self.name
@@ -49,22 +33,7 @@ class Subcategory(TimeStampMixin):
     is_active = models.BooleanField(default=True)
     is_hidden = models.BooleanField(default=False)
 
-    def save(self, *args, **kwargs):
-        if self.name:
-            self.name = self.name.strip()
-
-        if not self.slug:
-            base_slug = slugify(self.name)
-            slug = base_slug
-            counter = 1
-
-            while Subcategory.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-
-            self.slug = slug
-
-        super().save(*args, **kwargs)
+   
 
     def __str__(self):
         return self.name
