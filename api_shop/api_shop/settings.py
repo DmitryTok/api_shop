ccb3e7dc7bb6bef5a534127d2418f65a63b2ffa3
Dmytro <86673991+DmitryTok@users.noreply.github.com>
2026-06-29 21:10:53 +0200

    Remove DEFAULT_FILTER_BACKENDS from settings
    Removed DEFAULT_FILTER_BACKENDS from settings.

--- api_shop/api_shop/settings.py
+++ api_shop/api_shop/settings.py
@@ -149,11 +149,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    
-    "DEFAULT_FILTER_BACKENDS": [
-    "django_filters.rest_framework.DjangoFilterBackend"
-],
-
 }
 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
