220e39f7459644ec552c4e337201395ed337e20a
LIca72 <163239081+LIca72@users.noreply.github.com>
2026-09-16 11:33:24 +0300

    feat: add API pagination

--- api_shop/api_shop/settings.py
+++ api_shop/api_shop/settings.py
@@ -240,6 +240,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 15,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_RENDERER_CLASSES": [
